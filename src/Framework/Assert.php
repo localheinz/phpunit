a4c5b6316430271b6ec5d5f04b3110e1762a6a41
Andreas Möller <am@localheinz.com>
2020-12-18 21:57:58 +0100

    Enhancement: Emit Assertion\Made event
    Co-authored-by: Andreas Möller <am@localheinz.com>
    Co-authored-by: Arne Blankerts <Arne@Blankerts.de>

--- src/Framework/Assert.php
+++ src/Framework/Assert.php
@@ -33,6 +33,7 @@ use function strpos;
 use ArrayAccess;
 use Countable;
 use Generator;
+use PHPUnit\Event;
 use PHPUnit\Framework\Constraint\ArrayHasKey;
 use PHPUnit\Framework\Constraint\Callback;
 use PHPUnit\Framework\Constraint\ClassHasAttribute;
@@ -2078,7 +2079,11 @@ abstract class Assert
     {
         self::$count += count($constraint);
 
-        $constraint->evaluate($value, $message);
+        try {
+            $constraint->evaluate($value, $message);
+        } finally {
+            Event\Registry::emitter()->assertionMade();
+        }
     }
 
     /**
